da3a1091ebeb97fea8c7f1eeb707f670cdcec90f
Ahsung <dktjd2478@naver.com>
2020-06-28 05:12:24 +0900

    temp commit

--- app/src/main/java/com/example/vision_v0/MainActivity.java
+++ app/src/main/java/com/example/vision_v0/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1 = (Button) findViewById(R.id.gpsAct);
+        button2 = (Button) findViewById(R.id.cameraAct);
 
+        // GPS page Intent
         button1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),GpsActivity.class);
                 startActivity(intent);//액티비티 띄우기
-            } //button listener end
+            }
+        });
+
+        // CAMERA page Intent
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CameraActivity.class);
+                startActivity(intent);//액티비티 띄우기
+            }
         });
     }
 }
